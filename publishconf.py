345bf7e076ed60489d017c63db5bd11e6823b391
soringherghisan <sorin.gherghisan@gmail.com>
2025-04-18 16:22:26 +0300

    Set custom URL and save paths for articles, pages, and taxonomies.
    Configured ARTICLE_URL, PAGE_URL, CATEGORY_URL, TAG_URL, and AUTHOR_URL to use specific slug-based paths. This ensures cleaner and more consistent URLs for published content. Corresponding save paths were also updated to match these settings.

--- publishconf.py
+++ publishconf.py
@@ -11,6 +11,17 @@ from pelicanconf import *
 SITEURL = 'https://soringherghisan.github.io/pelican_blog'
 RELATIVE_URLS = False
 
+ARTICLE_URL = '{slug}.html'
+ARTICLE_SAVE_AS = '{slug}.html'
+PAGE_URL = '{slug}.html'
+PAGE_SAVE_AS = '{slug}.html'
+CATEGORY_URL = 'category/{slug}.html'
+CATEGORY_SAVE_AS = 'category/{slug}.html'
+TAG_URL = 'tag/{slug}.html'
+TAG_SAVE_AS = 'tag/{slug}.html'
+AUTHOR_URL = 'author/{slug}.html'
+AUTHOR_SAVE_AS = 'author/{slug}.html'
+
 # for feeds
 FEED_DOMAIN = SITEURL
 
